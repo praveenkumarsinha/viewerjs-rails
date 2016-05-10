f990741d590512ad28b3392e74c691c08a6a0362
Jon Lunsford <jon@capturethecastle.net>
2016-05-10 10:11:35 -0700

    Include pdfworker js file in manifest

--- lib/viewerjs/rails.rb
+++ lib/viewerjs/rails.rb
@@ -9,7 +9,7 @@ module Viewerjs
     class Railtie < ::Rails::Railtie
       #Adding pdf.worker.js to precompile set of assets otherwise developer(s)/user(s) are forced to append it in their
       #respective environment file
-      config.assets.precompile += %w( pdf.worker.js )
+      #config.assets.precompile += %w( pdf.worker.js )
 
       initializer "viewerjs.view_helpers" do
         ActionView::Base.send :include, Viewerjs::ViewHelpers
